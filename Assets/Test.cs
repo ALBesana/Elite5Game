79bd2e33a0a2dcab421097b9dda4213e58dd8867
Isacee <alfonsofavio28@gmail.com>
2024-10-02 20:18:04 +0800

    Added Test Script

---
+++ Assets/Test.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayerController : MonoBehaviour
+{
+    [Header("Horizontal Movement Settings")]
+    [SerializeField] private float walkSpeed = 1;
+
+    [Header("Ground Check Settings")]
+    [SerializeField] private float jumpForce = 45f;
+    [SerializeField] private Transform groundCheckpoint;
+    [SerializeField] private float groundCheckY = 0.2f;
+    [SerializeField] private float groundCheckX = 0.5f;
+    [SerializeField] private LayerMask IsGround;
+
+    private Rigidbody2D rb;
+    private float xAxis;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInputs();
+        Move();
+        Jump();
+    }
+
+    void GetInputs()
+    {
+        xAxis = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(walkSpeed * xAxis, rb.velocity.y);
+    }
+
+    public bool Grounded()
+    {
+        if (Physics2D.Raycast(groundCheckpoint.position, Vector2.down, groundCheckY, IsGround)
+            || Physics2D.Raycast(groundCheckpoint.position + new Vector3(groundCheckX, 0, 0), Vector2.down, groundCheckY, IsGround)
+            || Physics2D.Raycast(groundCheckpoint.position + new Vector3(-groundCheckX, 0, 0), Vector2.down, groundCheckY, IsGround))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && Grounded())
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce);
+        }
+    }
+}
