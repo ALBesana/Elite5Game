ca7645badc5c3e63b3ca1b29462ec31361cc9664
ALBesana <albesana2015@gmail.com>
2024-10-02 20:23:18 +0800

    Update S

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -15,7 +15,7 @@ public class PLayerController : MonoBehaviour
     [SerializeField] private LayerMask IsGround;
 
     private Rigidbody2D rb;
-    private float xAxiss;
+    private float xAxisss;
 
 
     // Start is called before the first frame update
